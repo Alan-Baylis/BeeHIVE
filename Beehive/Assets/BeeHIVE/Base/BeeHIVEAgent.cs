21bdf95ef87fbc09f25d38f97438be5f300279d5
Nilo Varela <nilofvgm@gmail.com>
2016-08-02 15:53:49 -0300

    First push

---
+++ Beehive/Assets/BeeHIVE/Base/BeeHIVEAgent.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using BeeHive;
+
+public class BeeHIVEAgent : MonoBehaviour {
+	
+	public ScriptableTree behaviorTreeObj;
+	public float tickRate = 0.06f;
+	BH_BehaviorTree tree;
+
+	// Use this for initialization
+	protected virtual void Start () {
+		InitBeeHive();
+	}
+
+	protected void InitBeeHive(){
+		tree = BeeHIVELoader.LoadTree(behaviorTreeObj);
+		tree.SetSource (this);
+		StartCoroutine ("RunBhvTree");
+	}
+
+	IEnumerator RunBhvTree(){
+		while(Application.isPlaying){			
+			yield return new WaitForSeconds (tickRate);
+			tree.Tick();
+		}
+	}
+}
